fe6b31495e2d1fe6b4c00d9992591c85b98d9d11
Ben Norris <ben@bsn.io>
2016-04-25 17:41:22 -0600

    Add empty framework

---
+++ FirebaseReSwift/FirebaseReSwift.h
@@ -0,0 +1,19 @@
+//
+//  FirebaseReSwift.h
+//  FirebaseReSwift
+//
+//  Created by Ben Norris on 4/25/16.
+//  Copyright © 2016 BSN Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FirebaseReSwift.
+FOUNDATION_EXPORT double FirebaseReSwiftVersionNumber;
+
+//! Project version string for FirebaseReSwift.
+FOUNDATION_EXPORT const unsigned char FirebaseReSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirebaseReSwift/PublicHeader.h>
+
+
